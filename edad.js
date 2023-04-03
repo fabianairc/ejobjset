6dae35a4252cf136bb201f1f0917d3d298818e38
fabianairc <fabianaramirezzz@gmail.com>
2023-04-03 20:49:27 -0300

    fd

--- edad.js
+++ edad.js
@@ -1,2 +1,21 @@
-const edad = prompt("dime tu edad");
-console.log(edad);
+const fun1 = () => {
+  return true;
+};
+
+setTimeout(function () {
+  console.log("Hola, Soy un promesa");
+}, 5000);
+
+function generadora(){
+    let id = 0;
+    while(true)
+    {
+     id++
+    yield id +1;
+
+    }
+}
+console.log(generadora());
+console.log(generadora());
+console.log(generadora());
+console.log(generadora());
